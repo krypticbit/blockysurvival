69994ad933921b196e40731147b138c34ca22ea6
Billy S <bbrus@comcast.net>
2018-06-06 15:50:28 -0400

    Fixed typo

--- open_ai/init.lua
+++ open_ai/init.lua
@@ -84,7 +84,7 @@ open_ai.register_mob = function(name,def)
 	open_ai.spawn_table[entity_name] = {}
 	open_ai.spawn_table[entity_name].spawn_node = def.spawn_node
 	open_ai.spawn_table[entity_name].liquid_mob = def.liquid_mob
-	open_ai.spawn_table[entity_name].min_percentage = def.spawn_node_percentage or 0
+	open_ai.spawn_table[entity_name].min_percent = def.spawn_node_percentage or 0
 	--store default collision box globally
 	open_ai.defaults[entity_name] = {}
 	open_ai.defaults[entity_name]["collisionbox"] = table.copy(def.collisionbox)
