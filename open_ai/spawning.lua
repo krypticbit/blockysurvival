d99c3e1738099366c48a05dea7a24d8509927391
Billy S <bbrus@comcast.net>
2018-06-07 16:08:33 -0400

    Fixed typo

--- open_ai/spawning.lua
+++ open_ai/spawning.lua
@@ -13,7 +13,7 @@ function get_suitable_spawn(pos1, pos2, def)
 	local data = vm:get_data()
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	local block_id = minetest.get_content_id(def.spawn_node)
-	for i_here in area:iter(pos1.x, pos1.y + 1, pos1.z, pos2.z, pos2.y - 1, pos2.z) do
+	for i_here in area:iter(pos1.x, pos1.y + 1, pos1.z, pos2.x, pos2.y - 1, pos2.z) do
 		minetest.log(tostring(i_here))
 		if data[i_here] == def.spawn_node then
 			local pos = area:position(i_here)
