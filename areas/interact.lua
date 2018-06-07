4a54dbe88c634cfa3a3561ca8b7393b15cd7bc37
Sires <ranearodrigo1@gmail.com>
2018-06-07 14:13:25 -0300

    Tweak protection violation a little bit

--- areas/interact.lua
+++ areas/interact.lua
@@ -20,12 +20,13 @@ minetest.register_on_protection_violation(function(pos, name)
 		player_pos = vector.round(player_pos)
 		if player_pos.y - 1 == pos then
 			if minetest.get_node(player_pos).name ~= "air" then
-				player_pos.y = player_pos.y - 0.2
+				player_pos.y = player_pos.y + 0.5
 			end
 			player:set_pos(player_pos)
 		end
 		player:set_look_horizontal((player:get_look_horizontal() + math.pi) % (math.pi * 2))
-		player:set_hp(1)
+		player:set_look_vertical((player:get_look_vertical() + math.pi) % (math.pi * 2))
+		player:set_hp(player:get_hp() - 1)
 	end
 end)
 
--- extra_functions/init.lua
+++
@@ -1,3 +0,0 @@
-path = minetest.get_modpath("extra_functions")
-
-dofile(path .. "/protection_violation.lua")
