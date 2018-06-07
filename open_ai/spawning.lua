0131a0c989891fed4124b0227c40245571897e35
Billy S <bbrus@comcast.net>
2018-06-07 15:46:16 -0400

    Chaged spawn detection method again

--- open_ai/spawning.lua
+++ open_ai/spawning.lua
@@ -6,6 +6,26 @@ open_ai.spawn_step = 0
 open_ai.spawn_timer = 70 --spawn every x seconds
 open_ai.spawn_table = {}
 
+function get_suitable_spawn(pos1, pos2, def)
+	blocks = {}
+	local vm = minetest.get_voxel_manip()
+	local emin, emax = vm:read_from_map(pos1, pos2)
+	local data = vm:get_data()
+	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+	local block_id = minetest.get_content_id(def.spawn_node)
+	for i_here in area:iter(pos1.x, pos1.y + 1, pos1.z, pos2.z, pos2.y - 1, pos2.z)
+		if data[i_here] == def.spawn_node then
+			local pos = area:position(i_here)
+			local i_above = area:index(pos.x, pos.y + 1, pos.z)
+			local walkable = mintest.registered_nodes[minetest.get_name_from_content_id(i_above)].walkable
+			if not walkable then
+				table.insert(blocks, pos)
+			end
+		end
+	end
+	return blocks
+end
+
 minetest.register_globalstep(function(dtime)
 	open_ai.spawn_step = open_ai.spawn_step + dtime
 	
@@ -17,22 +37,13 @@ minetest.register_globalstep(function(dtime)
 				for mob,def_table in pairs(open_ai.spawn_table) do
 					if math.random() < def_table.chance then
 						--minetest.log(mob,dump(def_table))
+						
 						--test for nodes to spawn mobs in
-						local test_for_node
-						if def_table.nodes_required then
-							test_for_node = minetest.find_nodes_in_area({x = pos.x - 20, y = pos.y - 20, z = pos.z - 20}, {x = pos.x + 20, y = pos.y + 20, z = pos.z + 20}, {def_table.spawn_node})
-							for i = 1, def_table.nodes_required do
-								for id, node in ipairs(test_for_node) do
-									local newnode = node
-									newnode.y = newnode.y - i
-									if minetest.get_node(node).name ~= def_table.spawn_node then
-										table.remove(test_for_node, id)
-									end
-								end
-							end
-						end
+						local test_for_node = get_suitable_spawn({x=pos.x-20,y=pos.y-20,z=pos.z-20}, {x=pos.x+20,y=pos.y+20,z=pos.z+20}, def_table)
+						--if the table has a node position then spawn the mob
+						local positions = table.getn(test_for_node)
 						--if position is above 0 then spawn node was found successfully 
-						if #test_for_node > def_table.min_percent * 4 then
+						if positions > def_table.min_percent * 4 then
 							--get a random node out of the table and add 1 y to it to spawn mob above it
 							--use the mob height eventually to spawn on the node exactly
 							local pos2 = test_for_node[math.random(1,positions)]
