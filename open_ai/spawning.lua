9fadbea6070b3d8c47fb4742f3ac451cb037a060
Billy S <bbrus@comcast.net>
2018-06-07 16:05:41 -0400

    Update spawning.lua

--- open_ai/spawning.lua
+++ open_ai/spawning.lua
@@ -13,10 +13,8 @@ function get_suitable_spawn(pos1, pos2, def)
 	local data = vm:get_data()
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	local block_id = minetest.get_content_id(def.spawn_node)
-	local x = 0
 	for i_here in area:iter(pos1.x, pos1.y + 1, pos1.z, pos2.z, pos2.y - 1, pos2.z) do
-		x = x + 1
-		minetest.log(tostring(x))
+		minetest.log(tostring(i_here))
 		if data[i_here] == def.spawn_node then
 			local pos = area:position(i_here)
 			local i_above = area:index(pos.x, pos.y + 1, pos.z)
