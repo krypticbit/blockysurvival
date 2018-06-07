ab849bd0b5affd496fd28a89b5751ced2d43c69f
Billy S <bbrus@comcast.net>
2018-06-06 20:56:25 -0400

    Made liquid mobs require 2 nodes deep to spawn

--- open_ai/spawning.lua
+++ open_ai/spawning.lua
@@ -6,6 +6,37 @@ open_ai.spawn_step = 0
 open_ai.spawn_timer = 70 --spawn every x seconds
 open_ai.spawn_table = {}
 
+function get_suitable_spawn(pos1, pos2, def)
+	blocks = {}
+	local vm = minetest.get_voxel_manip()
+	local emin, emax = vm:read_from_map(pos1, pos2)
+	local data = vm:get_data()
+	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+	local block_id = minetest.get_content_id(def.spawn_node)
+	local air_id = minetest.get_content_id("air")
+	for z = pos1.z, pos2.z do
+		for y = pos1.y + 1, pos2.y - 1 do -- Needs to be able to check if the node above is air, and sometimes if the node below is water
+			for x = pos1.x, pos2.x do
+				local voxel_ind = area:index(x, y, z)
+				if data[voxel_ind] == block_id then -- Mob spawns on block
+					local above_voxel_ind = area:index(x, y + 1, z)
+					if data[voxel_ind] == air_id then -- There is air above the block
+						if def.liquid_mob then -- If the mob swims
+							local below_voxel_ind = area:index(x, y - 1, z) -- Make sure the liquid is at least 2 nodes deep
+							if data[below_voxel_ind] == block_id then -- Liquid is at least two nodes deep
+								table.insert(blocks, {x = x, y = y, z = z})
+							end
+						else
+							table.insert(blocks, {x = x, y = y, z = z})
+						end
+					end
+				end
+			end
+		end
+	end
+	return blocks
+end
+
 minetest.register_globalstep(function(dtime)
 	open_ai.spawn_step = open_ai.spawn_step + dtime
 	
@@ -19,7 +50,7 @@ minetest.register_globalstep(function(dtime)
 						--print(mob,dump(def_table))
 						
 						--test for nodes to spawn mobs in
-						local test_for_node = minetest.find_nodes_in_area_under_air({x=pos.x-20,y=pos.y-20,z=pos.z-20}, {x=pos.x+20,y=pos.y+20,z=pos.z+20}, def_table.spawn_node)
+						local test_for_node = get_suitable_spawn({x=pos.x-20,y=pos.y-20,z=pos.z-20}, {x=pos.x+20,y=pos.y+20,z=pos.z+20}, def_table)
 						--if the table has a node position then spawn the mob
 						local positions = table.getn(test_for_node)
 						--if position is above 0 then spawn node was found successfully 
