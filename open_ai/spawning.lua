163ebbd76834554d77b096b59d74a488824bd116
Billy S <bbrus@comcast.net>
2018-06-06 20:07:35 -0400

    Added spawn chance

--- open_ai/spawning.lua
+++ open_ai/spawning.lua
@@ -15,22 +15,23 @@ minetest.register_globalstep(function(dtime)
 			if player:get_hp() > 0 then
 				local pos = player:getpos()
 				for mob,def_table in pairs(open_ai.spawn_table) do
-					
-					--print(mob,dump(def_table))
-					
-					--test for nodes to spawn mobs in
-					local test_for_node = minetest.find_nodes_in_area_under_air({x=pos.x-20,y=pos.y-20,z=pos.z-20}, {x=pos.x+20,y=pos.y+20,z=pos.z+20}, def_table.spawn_node)
-					--if the table has a node position then spawn the mob
-					local positions = table.getn(test_for_node)
-					--if position is above 0 then spawn node was found successfully 
-					if positions > def_table.min_percent * 4 then
-						--get a random node out of the table and add 1 y to it to spawn mob above it
-						--use the mob height eventually to spawn on the node exactly
-						local pos2 = test_for_node[math.random(1,positions)]
-						if def_table.liquid_mob ~= true then
-							pos2.y = pos2.y - open_ai.defaults[mob]["collisionbox"][2]
+					if math.random() < def_table.chance then
+						--print(mob,dump(def_table))
+						
+						--test for nodes to spawn mobs in
+						local test_for_node = minetest.find_nodes_in_area_under_air({x=pos.x-20,y=pos.y-20,z=pos.z-20}, {x=pos.x+20,y=pos.y+20,z=pos.z+20}, def_table.spawn_node)
+						--if the table has a node position then spawn the mob
+						local positions = table.getn(test_for_node)
+						--if position is above 0 then spawn node was found successfully 
+						if positions > def_table.min_percent * 4 then
+							--get a random node out of the table and add 1 y to it to spawn mob above it
+							--use the mob height eventually to spawn on the node exactly
+							local pos2 = test_for_node[math.random(1,positions)]
+							if def_table.liquid_mob ~= true then
+								pos2.y = pos2.y - open_ai.defaults[mob]["collisionbox"][2]
+							end
+							minetest.add_entity(pos2, mob)
 						end
-						minetest.add_entity(pos2, mob)
 					end
 					
 				end
