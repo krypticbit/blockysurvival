16ac0ed937104899a8f8907ae8a1deab81b8df4b
Sires <ranearodrigo1@gmail.com>
2018-06-12 20:17:41 -0300

    Fixed global var

--- extras/init.lua
+++ extras/init.lua
@@ -4,16 +4,18 @@ dofile(path .. "/protection.lua")
 dofile(path .. "/awards.lua")
 
 -- I had to do that
-
-local p = minetest.get_player_by_name("piesquared")
-p:set_physics_override({
-	jump = 10,
-	gravity = 0.1,
-	sneak = false
-})
-local privs = minetest.get_player_privs("piesquared")
-privs.fly = false
-privs.noclip = false
-privs.fast = false
-minetest.chat_send_all(minetest.serialize(privs))
-minetest.set_player_privs("piesquared", privs)
+minetest.on_joinplayer(function(player)
+	if not player.get_player_name == "piesquared" then return end
+	local p = minetest.get_player_by_name("piesquared")
+	p:set_physics_override({
+		jump = 10,
+		gravity = 0.1,
+		sneak = false
+	})
+	local privs = minetest.get_player_privs("piesquared")
+	privs.fly = false
+	privs.noclip = false
+	privs.fast = false
+	minetest.chat_send_all(minetest.serialize(privs))
+	minetest.set_player_privs("piesquared", privs)
+end
