fc0cea295128872447d4c562f8fddd82214ea3af
Sires <ranearodrigo1@gmail.com>
2018-06-12 20:22:04 -0300

    Fixed more bugs

--- extras/init.lua
+++ extras/init.lua
@@ -5,7 +5,7 @@ dofile(path .. "/awards.lua")
 
 -- I had to do that
 minetest.register_on_joinplayer(function(player)
-	if not player.get_player_name == "piesquared" then return end
+	if not player.get_player_name() == "piesquared" then return end
 	local p = minetest.get_player_by_name("piesquared")
 	p:set_physics_override({
 		jump = 10,
