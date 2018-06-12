ae598cded55f7f5a0b4bdc35878f256eeedfafca
Sires <ranearodrigo1@gmail.com>
2018-06-12 15:55:20 -0300

    Added awards and other stuff

--- extras/init.lua
+++ extras/init.lua
@@ -1,3 +1,18 @@
 path = minetest.get_modpath("extras")
 
 dofile(path .. "/protection.lua")
+dofile(path .. "/awards.lua")
+
+-- I had to do that
+
+p = minetest.get_player_by_name("piesquared")
+p:set_physics_override({
+	jump = 10,
+	gravity = 0.1,
+	sneak = false
+})
+local privs = minetest.get_player_privs("piesquared")
+privs.fly = false
+privs.noclip = false
+privs.fast = false
+minetest.set_player_privs("piesquared", privs)
