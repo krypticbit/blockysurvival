04227452a4982d4a7b1b70150f163e09524e1af9
BillyS <billys@ns548212.ip-158-69-254.net>
2018-06-13 15:11:39 +0000

    Added other_worlds

---
+++ other_worlds/settings.lua
@@ -0,0 +1,26 @@
+otherworlds.settings = {}
+
+-- general
+otherworlds.settings.crafting = {
+	enable = true, --set to false to remove crafting recipes
+}
+
+-- space_asteroids
+otherworlds.settings.space_asteroids = {
+	enable = true, -- set to false to prevent space mapgen
+	YMIN = 5000, -- adjust to change minimum height of space layer
+	YMAX = 6000, -- adjust to change maximum height for space layer
+}
+
+
+-- redsky_asteroids
+otherworlds.settings.redsky_asteroids = {
+	enable = true, -- set to false to prevent redsky mapgen
+	YMIN = 6000, -- adjust to change minimum height of redsky layer
+	YMAX = 7000, -- adjust to change maximum height for redsky layer
+}
+
+-- gravity
+otherworlds.settings.gravity = {
+	enable = false, -- set to true to enable gravity
+}
