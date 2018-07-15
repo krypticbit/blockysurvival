9993ed7e259d58040862679659a445114ed88d44
Billy S <bbrus@comcast.net>
2018-07-15 08:41:23 -0400

    Fixed bug

--- homedecor_modpack/homedecor/handlers/water_particles.lua
+++ homedecor_modpack/homedecor/handlers/water_particles.lua
@@ -30,8 +30,8 @@ function homedecor.start_particle_spawner(pos, node, particledef, soundname)
 			end, s_handle)
 		end
 		minetest.delete_particlespawner(id)
-		this_spawner_meta:set_int("active", nil)
-		this_spawner_meta:set_int("sound", nil)
+		this_spawner_meta:set_int("active", 0)
+		this_spawner_meta:set_int("sound", 0)
 		return
 	end
 
@@ -103,12 +103,12 @@ function homedecor.stop_particle_spawner(pos)
 		minetest.delete_particlespawner(id)
 	end
 
-	if s_handle then
+	if s_handle ~= 0 then
 		minetest.after(0, function(handle)
 			minetest.sound_stop(handle)
 		end, s_handle)
 	end
 
-	this_spawner_meta:set_int("active", nil)
-	this_spawner_meta:set_int("sound", nil)
+	this_spawner_meta:set_int("active", 0)
+	this_spawner_meta:set_int("sound", 0)
 end
