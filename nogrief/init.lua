60f427d392d6c85145f6ea261ab846c13f710e41
Billy S <bbrus@comcast.net>
2018-08-15 13:05:44 -0400

    Made it be triggered only on lava place

--- nogrief/init.lua
+++ nogrief/init.lua
@@ -6,7 +6,7 @@ minetest.item_place = function (itemstack, placer, pointed_thing, param2)
 			return
 	end
 	local player_name = placer:get_player_name()
-	if pointed_thing.above.y > -5 and not minetest.check_player_privs(player_name, {lava = true}) then
+	if itemstack:get_name() == "default:lava_source" and pointed_thing.above.y > -5 and not minetest.check_player_privs(player_name, {lava = true}) then
 		minetest.chat_send_player(player_name, "You cannot place lava over -5m", true)
 		return itemstack
 	end
