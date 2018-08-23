e6604a8cefa20d0cd8acf429ccd16704e3c5ed7c
Billy-S <bbrus@comcast.net>
2018-08-23 12:12:55 -0400

    Fixed no_guests regex again

--- no_guests/init.lua
+++ no_guests/init.lua
@@ -1,5 +1,6 @@
 minetest.register_on_prejoinplayer(function(name, ip)
-	if name:find("%u.*%d%d$") then
+	if name:find("%u.*%d%d%d$") then
+		minetest.log("Player with name " .. name .. " was blocked by no_guests")
 		return "Please change your name to not end in numbers"
 	end
 end)
