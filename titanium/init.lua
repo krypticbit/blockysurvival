3c49645053e74bd13540ad42fb4244b364290749
Billy S <bbrus@comcast.net>
2018-08-25 18:11:27 -0400

    Improved titanium google glass method

--- titanium/init.lua
+++ titanium/init.lua
@@ -264,107 +264,46 @@ minetest.register_ore({
 
 if enable_walking_light then
 
-	local players = {}
-	local player_positions = {}
-	local last_wielded = {}
-
+	local lights = {}
+	local timer = 0
+	local update = 0.2
+	
 	function round(num) 
 		return math.floor(num + 0.5) 
 	end
 
 	function check_for_googles (player)
-	if player==nil then return false end
-	local inv = player:get_inventory()
-	local hotbar=inv:get_list("main")
-	for index=1,8,1 do
-		if hotbar[index]:get_name() == "titanium:sam_titanium" then
-			return "titanium:sam_titanium"
-		end
-	end
-	return false
-	end   
-
-	minetest.register_on_joinplayer(function(player)
-		local player_name = player:get_player_name()
-		table.insert(players, player_name)
-		last_wielded[player_name] = player:get_wielded_item():get_name()
-		local pos = player:getpos()
-		local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
-		local wielded_item = check_for_googles(player)
-		if wielded_item ~= "titanium:sam_titanium" then
-			minetest.add_node(rounded_pos,{type="node",name="titanium:who_knows"})
-			minetest.add_node(rounded_pos,{type="node",name="air"})
-		end
-		player_positions[player_name] = {}
-		player_positions[player_name]["x"] = rounded_pos.x;
-		player_positions[player_name]["y"] = rounded_pos.y;
-		player_positions[player_name]["z"] = rounded_pos.z;
-	end)
-
-	minetest.register_on_leaveplayer(function(player)
-		local player_name = player:get_player_name()
-		for i,v in ipairs(players) do
-			if v == player_name then 
-				table.remove(players, i)
-				last_wielded[player_name] = nil
-				local pos = player:getpos()
-				local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
-				minetest.add_node(rounded_pos,{type="node",name="titanium:who_knows"})
-				minetest.add_node(rounded_pos,{type="node",name="air"})
-				player_positions[player_name]["x"] = nil
-				player_positions[player_name]["y"] = nil
-				player_positions[player_name]["z"] = nil
-				player_positions[player_name]["m"] = nil
-				player_positions[player_name] = nil
+		if player == nil then return false end
+		local inv = player:get_inventory()
+		local hotbar = inv:get_list("main")
+		for index=1,8,1 do
+			if hotbar[index]:get_name() == "titanium:sam_titanium" then
+				return true
 			end
 		end
-	end)
-
+			return false
+	end
+	
 	minetest.register_globalstep(function(dtime)
-		for i,player_name in ipairs(players) do
+		timer = timer + dtime
+		players = minetest.get_connected_players()
+		for i, player_name in ipairs(players) do
 			local player = minetest.get_player_by_name(player_name)
-			local wielded_item = check_for_googles(player)
-			if wielded_item == "titanium:sam_titanium" then
-				local pos = player:getpos()
-				local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
-				if (last_wielded[player_name] ~= "titanium:sam_titanium") or (player_positions[player_name]["x"] ~= rounded_pos.x or player_positions[player_name]["y"] ~= rounded_pos.y or player_positions[player_name]["z"] ~= rounded_pos.z) then
-					local is_air  = minetest.get_node_or_nil(rounded_pos)
-					if is_air == nil or (is_air ~= nil and (is_air.name == "air" or is_air.name == "titanium:light")) then
-						minetest.add_node(rounded_pos,{type="node",name="titanium:light"})
-					end
-					if (player_positions[player_name]["x"] ~= rounded_pos.x or player_positions[player_name]["y"] ~= rounded_pos.y or player_positions[player_name]["z"] ~= rounded_pos.z) then
-						local old_pos = {x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]}
-						local is_light = minetest.get_node_or_nil(old_pos)
-						if is_light ~= nil and is_light.name == "titanium:light" then
-							minetest.add_node(old_pos,{type="node",name="titanium:who_knows"})
-							minetest.add_node(old_pos,{type="node",name="air"})
-						end
-					end
-					player_positions[player_name]["x"] = rounded_pos.x
-					player_positions[player_name]["y"] = rounded_pos.y
-					player_positions[player_name]["z"] = rounded_pos.z
+			local has_googles = check_for_googles(player)
+			if has_googles then
+				let pos = player:get_pos()
+				local nodepos = {x = round(pos.x), y = round(pos.y), z = round(pos.z)}
+				let n = minetest.get_node(nodepos)
+				if n.name == "air" then
+					minetest.set_node({name = "titanium:light"})
+					table.insert(lights, nodepos)
 				end
-
-				last_wielded[player_name] = wielded_item;
-			elseif last_wielded[player_name] == "titanium:sam_titanium" then
-				local pos = player:getpos()
-				local rounded_pos = {x=round(pos.x),y=round(pos.y)+1,z=round(pos.z)}
-				repeat
-					local is_light  = minetest.get_node_or_nil(rounded_pos)
-					if is_light ~= nil and is_light.name == "titanium:light" then
-						minetest.add_node(rounded_pos,{type="node",name="titanium:who_knows"})
-						minetest.add_node(rounded_pos,{type="node",name="air"})
-					end
-				until minetest.get_node_or_nil(rounded_pos) ~= "titanium:light"
-				local old_pos = {x=player_positions[player_name]["x"], y=player_positions[player_name]["y"], z=player_positions[player_name]["z"]}
-				repeat
-					is_light  = minetest.get_node_or_nil(old_pos)
-					if is_light ~= nil and is_light.name == "titanium:light" then
-						minetest.add_node(old_pos,{type="node",name="titanium:who_knows"})
-						minetest.add_node(old_pos,{type="node",name="air"})
-					end
-				until minetest.get_node_or_nil(old_pos) ~= "titanium:light"
-				last_wielded[player_name] = wielded_item
+			end
+		end
+		if timer > update then
+			timer = 0
+			for _, p in pairs(nodepos) do
+				minetest.set_node(p, {name = "air"})
 			end
 		end
 	end)
@@ -407,13 +346,6 @@ if enable_walking_light then
 			{'titanium:glass', 'default:mese_crystal', 'titanium:glass'},
 			{'', '', ''},
 		}
-	})
-	--------------------------------------------------------
-	minetest.register_node("titanium:who_knows", {
-		description = "?Who Knows?",
-		tiles = {"titanium.png"},
-		is_ground_content = true,
-		groups = {not_in_creative_inventory=1},
 	})
 end
 
